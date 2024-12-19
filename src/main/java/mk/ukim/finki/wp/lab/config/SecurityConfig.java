4454744bc8cd9cda1948684f499d717c648af2ed
MichaelSK <mihailsekuloski@gmail.com>
2024-12-19 20:42:00 +0100

    Added login/logout authorization.

---
+++ src/main/java/mk/ukim/finki/wp/lab/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package mk.ukim.finki.wp.lab.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    // In-Memory Authentication setup
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/songs/edit/**", "/songs/add").authenticated()
+                        .anyRequest().permitAll()
+                )
+                .formLogin(form -> form
+                        .defaultSuccessUrl("/songs", true)
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/logout").logoutSuccessUrl("/songs")
+                );
+
+        return http.build();
+    }
+
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth
+            .inMemoryAuthentication()
+            .withUser("user")
+            .password("password")
+            .roles("ADMIN");
+}
+
+    @Bean
+    @SuppressWarnings("deprecation") // For demo purposes only (NoOpPasswordEncoder is insecure)
+    public static NoOpPasswordEncoder passwordEncoder() {
+        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+    }
+}
